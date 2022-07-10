4271b320dc4b5ca601e2fb0d7c5e81bf44f0653d
masaki-nakamori <gauss.1994.ilovephys@gmail.com>
2022-07-10 17:01:44 +0900

    チャレンジ課題　旅行プログラム修正

--- trip_program.rb
+++ trip_program.rb
@@ -11,35 +11,46 @@ def choose_plan(plans)
     plans.each_with_index do |plan, i|
         puts "#{i+1}. #{plan[:destination]}（#{plan[:price]}円）"
     end
-    print "プラン番号を選択 > " 
-    @slelect_num = gets.to_i - 1
+    #異常入力の時は再入力
+    while true
+        print "プランの番号を選択 > "
+        select_num = gets.to_i - 1
+        break if(0..2).include?(select_num)
+        puts"1~3の番号を入力してください"
+    end
+    select_num
 end
 
 #選択プランの確認と各プランの金額計算
-def check_plan(plans)
-    @price_tour = plans[@slelect_num][:price]
-    puts "#{plans[@slelect_num][:destination]}ですね。"
+def check_plan(plans,select_num)
+    puts "#{plans[select_num][:destination]}ですね。"
+    price_tour = plans[select_num][:price]
 end
 
 #予約人数の確認
-def define_people_num
+def define_people_num()
     puts "何名で予約されますか？"
-    print "人数を入力 > "
-    @people_num = gets.to_i
+    while true
+        print "人数を入力 > "
+        people_num = gets.to_i
+        break if people_num >= 1
+        puts "1名以上選んでください。"
+    end
+    people_num
 end
 
 #金額計算
-def calculate_total_price(plans)
-    puts "#{@people_num}名ですね。"
-    tortal_price = (@price_tour * @people_num).round
-    if @people_num >= 5
+def calculate_total_price(plans,people_num, price_tour)
+    puts "#{people_num}名ですね。"
+    tortal_price = price_tour * people_num
+    if people_num >= 5
         tortal_price *= 0.9
         puts "5名以上ですので10%割引となります"
     end
     puts "合計金額は#{tortal_price.round}円になります。"
 end
 
-choose_plan(plans)
-check_plan(plans)
-define_people_num()
-calculate_total_price(plans)
+select_num = choose_plan(plans)
+price_tour = check_plan(plans,select_num)
+people_num = define_people_num()
+calculate_total_price(plans, people_num, price_tour)
